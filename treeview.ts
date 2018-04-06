91be06ba8b6c7e10461f886aa08e547fd60c11f7
Larry <lkmylin@gmail.com>
2018-04-05 17:47:10 -0700

    Added state caching

--- treeview.ts
+++ treeview.ts
@@ -2,11 +2,12 @@
 
   const treeviewModule = angular.module("treeviewModule", []);
 
-  treeviewModule.service("treeviewProvider", [() => {
-    this.GetInstance = (items: Array<TreeItem>) => {
-      return new Treeview(items);
-    };
-    return this;
+  treeviewModule.service("treeviewProvider", ["cacheServiceProvider", (cacheServiceProvider: CacheServiceProvider) : TreeviewProvider => {
+    return new TreeviewProvider(cacheServiceProvider.GetStateManagerInstance());
+  }]);
+
+  treeviewModule.service("cacheServiceProvider", ["$window", ($window: any) : CacheServiceProvider => {
+    return new CacheServiceProvider($window);
   }]);
 
   treeviewModule.directive("treeview", () => {
@@ -29,9 +30,20 @@
     };
   });
 
+  class TreeviewProvider {
+    GetInstance: any;
+    constructor(stateManager: StateManager) {
+      this.GetInstance = (id: string, treeItems: Array<TreeItem>) => {
+        return new Treeview(id, treeItems, stateManager);
+      }
+    }
+  }
+
   class Treeview {
+    ID: string;
     TreeItems: Array<TreeItem>;
     Collapsed: boolean;
+    StateManager: StateManager;
     ToggleAll(): void {
       const context = this;
       const toggleChildren = (items : Array<TreeItem>) => {
@@ -40,22 +52,33 @@
           toggleChildren(item.Children);
         });
       };
-      this.Collapsed = !this.Collapsed;
-      toggleChildren(this.TreeItems);
+      context.Collapsed = !context.Collapsed;      
+      context.StateManager.SetValue(context.ID, context.StateManager.CachedProperties.AllCollapsed, context.Collapsed);
+      context.StateManager.SetValue(context.ID, context.StateManager.CachedProperties.ExpandedNodes, []);
+      toggleChildren(context.TreeItems);
     };
-    constructor (items : Array<TreeItem>) {
+    IsNodeCollapsed(treeKey: number): boolean {
+      const expandedNodes = this.StateManager.GetValue(this.ID, this.StateManager.CachedProperties.ExpandedNodes, []);
+      if (expandedNodes.length === 0) return this.Collapsed;
+      const filteredExpandedNodes = expandedNodes.filter((tk: number) => tk === treeKey);
+      return filteredExpandedNodes.length === 0;
+    };
+    constructor (id: string, items : Array<TreeItem>, stateManager: StateManager) {
       const context = this;
-      this.TreeItems = [];
-      this.Collapsed = true;
+      context.ID = id;
+      context.TreeItems = [];
+      context.Collapsed = stateManager.GetValue(id, stateManager.CachedProperties.AllCollapsed, true);
+      context.StateManager = stateManager;
       items.forEach(item => {
         if (item.ParentKey === 0) {
-          context.TreeItems[context.TreeItems.length] = new TreeItem(item, items);
+          context.TreeItems[context.TreeItems.length] = new TreeItem(item, items, context);
         }
       });
     }
   }
 
   class TreeItem {
+    RootScope: Treeview;
     TreeKey: number;
     ParentKey: number;
     Title: string;
@@ -63,22 +86,69 @@
     HasChildren: boolean;
     Children: Array<TreeItem>;
     ToggleNode(): void {
-      this.Collapsed = !this.Collapsed;
+      const context = this;
+      context.Collapsed = !context.Collapsed;
+      var expandedNodes = context.RootScope.StateManager.GetValue(context.RootScope.ID, context.RootScope.StateManager.CachedProperties.ExpandedNodes, []);
+      expandedNodes = expandedNodes.filter((tk: number) => tk !== context.TreeKey);
+      if (!context.Collapsed) {
+        expandedNodes[expandedNodes.length] = context.TreeKey;
+      }
+      context.RootScope.StateManager.SetValue(context.RootScope.ID, context.RootScope.StateManager.CachedProperties.ExpandedNodes, expandedNodes);
     };
-    constructor (item: TreeItem, items: Array<TreeItem>) {
-      this.TreeKey = item.TreeKey;
-      this.ParentKey = item.ParentKey;
-      this.Title = item.Title;
-      this.Collapsed = true;
-      this.HasChildren = false;
-      this.Children = [];
+    constructor (item: TreeItem, items: Array<TreeItem>, rootScope: Treeview) {
+      const context = this;
+      context.RootScope = rootScope;
+      context.TreeKey = item.TreeKey;
+      context.ParentKey = item.ParentKey;
+      context.Title = item.Title;
+      context.Collapsed = rootScope.IsNodeCollapsed(item.TreeKey);
+      context.HasChildren = false;
+      context.Children = [];
       items.forEach(i => {
         if (i.ParentKey === item.TreeKey) {
-          this.HasChildren = true;
-          this.Children[this.Children.length] = new TreeItem(i, items);
+          context.HasChildren = true;
+          context.Children[context.Children.length] = new TreeItem(i, items, rootScope);
         }
       });
     }
+  }
+
+  class CacheServiceProvider {
+    GlobalScope: any;
+    GetStateManagerInstance() : StateManager {
+      return new StateManager(this.GlobalScope);
+    }
+    constructor(globalScope: any) {
+      this.GlobalScope = globalScope;
+    }
+  }
+
+  class StateManager {
+    GlobalScope: any;
+    CachedProperties: ICachedProperties;
+    CurrentState: any;
+    GetValue(controlID: string, property: string, defaultValue: any): any {
+      if (!this.CurrentState[controlID]) this.CurrentState[controlID] = {};
+      return typeof this.CurrentState[controlID][property] === "undefined" ? defaultValue : this.CurrentState[controlID][property];
+    };
+    SetValue(controlID: string, property: string, value: any): void {
+      if (!this.CurrentState[controlID]) this.CurrentState[controlID] = {};
+      this.CurrentState[controlID][property] = value;
+      if (this.GlobalScope.localStorage) this.GlobalScope.localStorage.TreeviewCache = JSON.stringify(this.CurrentState);
+    };
+    constructor(globalScope: any) {
+      this.GlobalScope = globalScope;
+      this.CurrentState = globalScope.localStorage && globalScope.localStorage.TreeviewCache ? JSON.parse(globalScope.localStorage.TreeviewCache) : {};
+      this.CachedProperties = {
+        AllCollapsed: "AllCollapsed",
+        ExpandedNodes: "ExpandedNodes"
+      }
+    }
+  }
+
+  interface ICachedProperties {
+    AllCollapsed: string;
+    ExpandedNodes: string;
   }
 
 })();
